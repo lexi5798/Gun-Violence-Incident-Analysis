5a0394b2219eb95158373e56b4140ed1ee4b5969
Alexis Johnson <149542040+lexi5798@users.noreply.github.com>
2026-07-30 20:35:17 -0700

    Update and rename gun_violence_incident_analysis.sql to basic_analysis.sql

--- scripts/basic_analysis.sql
+++ scripts/basic_analysis.sql
@@ -20,13 +20,13 @@ FROM clean_gun_violence;
 SELECT 
     COUNT(incident_id) AS incident_total
 FROM clean_gun_violence
-WHERE n_killed > 0
+WHERE n_killed > 0;
 
 -- Total mass shooting incidents (3+ killed, FBI definition, excluding shooter not accounted for in this dataset)
 SELECT 
     COUNT(incident_id) AS incident_total
 FROM clean_gun_violence
-WHERE n_killed >= 3
+WHERE n_killed >= 3;
 
 -- Total incidents by state
 SELECT 
@@ -34,11 +34,11 @@ SELECT
     COUNT(incident_id) AS incident_total
 FROM clean_gun_violence
 GROUP BY incident_state
-ORDER BY incident_total DESC
+ORDER BY incident_total DESC;
 
 -- Total casualities (n_killed + n_injured) 
 SELECT
     SUM(n_killed) AS total_killed,
     SUM(n_injured) AS total_injured,
     SUM(n_killed + n_injured) AS total_casualties
-FROM clean_gun_violence
+FROM clean_gun_violence;
